22da81f2be97c9ee17de53c9a4ea20e98d03428f
sunnyvedwal <sunnyvedwal@gmail.com>
2022-08-28 15:21:06 +0530

    file structure

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import { Actors, NavBar, Movies, MovieInformation, Profile } from '.';
+import useStyles from './styles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+
+          <Route exact path="/profile" element={<Profile />} />
+
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+
+          <Route exact path="/actors/:id" element={<Actors />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Button, IconButton, Avatar, Toolbar, Drawer } from '@mui/material';
+import { Menu, AccountCirlce, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  );
+};
+
+export default NavBar;
